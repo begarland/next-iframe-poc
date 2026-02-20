72047b1bef3ea874edfd0b6a18c8c1457aec878d
Brandy Garland <brandy.e.garland@gmail.com>
2026-02-19 21:17:00 -0700

    black text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import ContentfulPushContent from "./components/ContentfulPushContent";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
       <div className="flex w-screen flex-col md:flex-row justify-center items-center">
         <div className="w-1/2">
           <ContentfulPushContent />
