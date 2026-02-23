e486179fd615d2a09bd53ab1284f904b48481b80
Brandy Garland <brandy.e.garland@gmail.com>
2026-02-22 21:46:32 -0700

    comment out to figure out what is broken

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 // import ContentfulPullContent from "./components/ContentfulPullContent";
-// import ContentfulPushContent from "./components/ContentfulPushContent";
+import ContentfulPushContent from "./components/ContentfulPushContent";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
       <div className="flex w-screen flex-col md:flex-row justify-center items-start">
         <div className="w-1/2">
-          {/* <ContentfulPushContent /> */}
+          <ContentfulPushContent />
         </div>
         <div className="w-1/2">
           {/* <ContentfulPullContent /> */}
